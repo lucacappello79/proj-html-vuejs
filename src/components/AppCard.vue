a1b6451e8d2c2bca485925430207503dde4c6c42
lucacappello79 <postt79@gmail.com>
2023-04-11 12:27:06 +0200

    feat: adding componenets to scaffolding

---
+++ src/components/AppCard.vue
@@ -0,0 +1,34 @@
+<script>
+
+import { store } from "../store.js";
+
+export default {
+
+    name: "AppCard",
+
+    data() {
+
+        return {
+
+            store,
+
+        }
+    },
+
+};
+
+</script>
+
+
+<template>
+    <div class="card-wrapper">
+        <img src="" alt="">
+
+    </div>
+</template>
+
+
+  
+<style lang="scss" scoped>
+@use "../scss/variables" as *;
+</style>
